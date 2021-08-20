922ee5a61769cea57ffa76e98d3a1773844e0d07
Katarina <katarinadjordjeradojicic@gmail.com>
2021-08-20 20:52:21 +0200

    delete unnecessary lines

--- Repository/DbModels/User.cs
+++ Repository/DbModels/User.cs
@@ -8,7 +8,7 @@ namespace Repository.DbModels
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
-        public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+        public DateTime EnrollmentDate { get; set; }
         public string Email { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
